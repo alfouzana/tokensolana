6662fd6646e6f882c962cc583b7254cfeb6703c7
Abdullah Alfouzan <alfouzana@me.com>
2024-11-03 17:46:59 +0300

    Update SolanaCreateToken.jsx

--- Components/Solana/SolanaCreateToken.jsx
+++ Components/Solana/SolanaCreateToken.jsx
@@ -1,38 +1,3 @@
-import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import {
-  Keypair,
-  PublicKey,
-  SystemProgram,
-  Transaction,
-  LAMPORTS_PER_SOL,
-  TransactionSignature,
-} from "@solana/web3.js";
-import {
-  MINT_SIZE,
-  TOKEN_PROGRAM_ID,
-  createInitializeMintInstruction,
-  getMinimumBalanceForRentExemptMint,
-  getAssociatedTokenAddress,
-  createMintToInstruction,
-  createAssociatedTokenAccountInstruction,
-} from "@solana/spl-token";
-import {
-  createCreateMetadataAccountInstruction,
-  PROGRAM_ID,
-  createCreateMetadataAccountV3Instruction,
-} from "@metaplex-foundation/mpl-token-metadata";
-import axios from "axios";
-import React, { useEffect, useState, useCallback } from "react";
-import toast from "react-hot-toast";
-import { MdOutlineGeneratingTokens } from "react-icons/md";
-import { useRouter } from "next/router";
-
-//INTERNAL IMPORT
-import useUserSOLBalanceStore from "../../solana/stores/useUserSOLBalanceStore";
-import { useNetworkConfiguration } from "../../solana/contexts/NetworkConfigurationProvider";
-import UploadICON from "../Ethereum/SVG/UploadICON";
-import Input from "../Ethereum/Input";
-
 const SolanaCreateToken = ({
   setOpenSolanaTokenCreator,
   setLoader,
@@ -67,7 +32,10 @@ const SolanaCreateToken = ({
     supply: "",
     image: "",
     description: "",
+    revokeFreeze: false,
+    revokeMint: false,
   });
+
   const notifySuccess = (msg) => toast.success(msg, { duration: 2000 });
   const notifyError = (msg) => toast.error(msg, { duration: 2000 });
 
@@ -147,6 +115,19 @@ const SolanaCreateToken = ({
             ),
             createMetadataInstruction
           );
+
+          // Check if Revoke Freeze is selected
+          if (token.revokeFreeze) {
+            // Add revoke freeze logic here if needed
+            console.log("Revoke Freeze authority is selected");
+          }
+
+          // Check if Revoke Mint is selected
+          if (token.revokeMint) {
+            // Add revoke mint logic here if needed
+            console.log("Revoke Mint authority is selected");
+          }
+
           const signature = await sendTransaction(
             createNewTokenTransaction,
             connection,
@@ -154,7 +135,7 @@ const SolanaCreateToken = ({
               signers: [mintKeypair],
             }
           );
-          //STORE TOKEN
+
           if (mintKeypair.publicKey.toString()) {
             const today = Date.now();
             let date = new Date(today);
@@ -203,110 +184,6 @@ const SolanaCreateToken = ({
     [publicKey, connection, sendTransaction]
   );
 
-  const handleImageChange = async (event) => {
-    const file = event.target.files[0];
-    console.log(file);
-    if (file) {
-      const imgUrl = await uploadImagePinata(file);
-      updateToken({ ...token, image: imgUrl });
-    }
-  };
-
-  //---UPLOAD TO IPFS FUNCTION
-  const uploadImagePinata = async (file) => {
-    if (file) {
-      try {
-        setLoader(true);
-        const formData = new FormData();
-        formData.append("file", file);
-
-        const response = await axios({
-          method: "post",
-          url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
-          data: formData,
-          headers: {
-            pinata_api_key: PINATA_AIP_KEY,
-            pinata_secret_api_key: PINATA_SECRECT_KEY,
-            "Content-Type": "multipart/form-data",
-          },
-        });
-        const ImgHash = `https://gateway.pinata.cloud/ipfs/${response.data.IpfsHash}`;
-        setLoader(false);
-        return ImgHash;
-      } catch (error) {
-        setLoader(false);
-        notifyError("Upload image failed");
-      }
-      setLoader(false);
-    }
-  };
-
-  const uploadMetadata = async (token) => {
-    const { name, symbol, description, image, supply } = token;
-    console.log(name, symbol, description, image);
-
-    if (!name || !symbol || !description || !image)
-      return console.log("Data Missing");
-
-    const data = JSON.stringify({
-      name: name,
-      symbol: symbol,
-      description: description,
-      image: image,
-      supply: supply,
-    });
-
-    try {
-      const response = await axios({
-        method: "POST",
-        url: "https://api.pinata.cloud/pinning/pinJSONToIPFS",
-        data: data,
-        headers: {
-          pinata_api_key: PINATA_AIP_KEY,
-          pinata_secret_api_key: PINATA_SECRECT_KEY,
-          "Content-Type": "application/json",
-        },
-      });
-
-      const url = `https://gateway.pinata.cloud/ipfs/${response.data.IpfsHash}`;
-
-      return url;
-    } catch (error) {
-      setLoader(false);
-      notifyError("Upload image failed");
-    }
-  };
-
-  //FEE
-  const chargeFee = useCallback(async () => {
-    if (!publicKey) {
-      notifyError(`Send Transaction: Wallet not connected!`);
-      console.log("error", `Send Transaction: Wallet not connected!`);
-      return;
-    }
-    setLoader(true);
-    const creatorAddress = new PublicKey(SOLANA_RECEIVER);
-    let signature = "";
-
-    try {
-      const transaction = new Transaction().add(
-        SystemProgram.transfer({
-          fromPubkey: publicKey,
-          toPubkey: creatorAddress,
-          lamports: LAMPORTS_PER_SOL * Number(SOLANA_FEE),
-        })
-      );
-
-      signature = await sendTransaction(transaction, connection);
-      notifySuccess("Transaction successful!");
-      return signature;
-    } catch (error) {
-      setLoader(false);
-      notifyError(`Transaction failed! ${error?.message}`);
-      return;
-    }
-  }, [publicKey, SOLANA_FEE, sendTransaction, connection]);
-
   return (
     <div className="bootstrap">
       <div className="modal show" style={{ display: "block" }}>
@@ -323,96 +200,123 @@ const SolanaCreateToken = ({
               </div>
             </div>
             <div className="modal-body">
-              <>
-                {token.image ? (
-                  <div>
-                    <img
-                      style={{ width: "150px", height: "auto" }}
-                      src={token.image}
-                      alt=""
-                    />
+              {/* Image Upload Section */}
+              {token.image ? (
+                <div>
+                  <img
+                    style={{ width: "150px", height: "auto" }}
+                    src={token.image}
+                    alt=""
+                  />
+                </div>
+              ) : (
+                <label htmlFor="file" className="custum-file-upload">
+                  <div className="icon">
+                    <UploadICON />
                   </div>
-                ) : (
-                  <div>
-                    <label for="file" class="custum-file-upload">
-                      <div class="icon">
-                        <UploadICON />
-                      </div>
-                      <div class="text">
-                        <span>Click to upload Logo</span>
-                      </div>
-                      <input
-                        onChange={handleImageChange}
-                        id="file"
-                        type="file"
-                      />
-                    </label>
+                  <div className="text">
+                    <span>Click to upload Logo</span>
                   </div>
-                )}
-              </>
-              <div>
-                <Input
-                  icon={<MdOutlineGeneratingTokens />}
-                  placeholder={"Name"}
-                  handleChange={(e) =>
-                    updateToken({ ...token, name: e.target.value })
-                  }
-                />
-                <Input
-                  icon={<MdOutlineGeneratingTokens />}
-                  placeholder={"Symbol"}
-                  handleChange={(e) =>
-                    updateToken({ ...token, symbol: e.target.value })
-                  }
-                />
-                <Input
-                  icon={<MdOutlineGeneratingTokens />}
-                  placeholder={"Supply"}
-                  handleChange={(e) =>
-                    updateToken({ ...token, supply: e.target.value })
-                  }
-                />
-                <Input
-                  icon={<MdOutlineGeneratingTokens />}
-                  placeholder={"Decimals"}
-                  handleChange={(e) =>
-                    updateToken({ ...token, decimals: e.target.value })
-                  }
-                />
-                <Input
-                  icon={<MdOutlineGeneratingTokens />}
-                  placeholder={"Description"}
-                  handleChange={(e) =>
-                    updateToken({ ...token, description: e.target.value })
-                  }
-                />
-                {tokenMintAddress ? (
-                  <div className="form-group">
-                    <a
-                      href={`https://solscan.io/account/${tokenMintAddress}?cluster=devnet`}
-                      target="_blank"
-                      className="btn btn-primary btn-block "
-                    >
-                      View Explorer
-                    </a>
-                  </div>
-                ) : balance < 2 ? (
-                  <div className="form-group">
-                    <button className="btn btn-primary btn-block ">
-                      Balance {balance} Sol, required minimum 2 Sol
-                    </button>
-                  </div>
-                ) : (
-                  <div className="form-group">
-                    <button
-                      onClick={() => createToken(token)}
-                      className="btn btn-primary btn-block "
-                    >
-                      Create token (Fee {SOLANA_FEE} Sol)
-                    </button>
-                  </div>
-                )}
+                  <input
+                    onChange={handleImageChange}
+                    id="file"
+                    type="file"
+                  />
+                </label>
+              )}
+
+              {/* Input Fields */}
+              <Input
+                icon={<MdOutlineGeneratingTokens />}
+                placeholder={"Name"}
+                handleChange={(e) =>
+                  updateToken({ ...token, name: e.target.value })
+                }
+              />
+              <Input
+                icon={<MdOutlineGeneratingTokens />}
+                placeholder={"Symbol"}
+                handleChange={(e) =>
+                  updateToken({ ...token, symbol: e.target.value })
+                }
+              />
+              <Input
+                icon={<MdOutlineGeneratingTokens />}
+                placeholder={"Supply"}
+                handleChange={(e) =>
+                  updateToken({ ...token, supply: e.target.value })
+                }
+              />
+              <Input
+                icon={<MdOutlineGeneratingTokens />}
+                placeholder={"Decimals"}
+                handleChange={(e) =>
+                  updateToken({ ...token, decimals: e.target.value })
+                }
+              />
+              <Input
+                icon={<MdOutlineGeneratingTokens />}
+                placeholder={"Description"}
+                handleChange={(e) =>
+                  updateToken({ ...token, description: e.target.value })
+                }
+              />
+
+              {/* Checkboxes for Revoke Freeze and Revoke Mint */}
+              <div className="form-group">
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={token.revokeFreeze}
+                    onChange={(e) =>
+                      updateToken({ ...token, revokeFreeze: e.target.checked })
+                    }
+                  />
+                  Revoke Freeze (required)
+                  <p>Revoke Freeze allows you to create a liquidity pool</p>
+                </label>
               </div>
+              <div className="form-group">
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={token.revokeMint}
+                    onChange={(e) =>
+                      updateToken({ ...token, revokeMint: e.target.checked })
+                    }
+                  />
+                  Revoke Mint
+                  <p>Mint Authority allows you to increase token supply</p>
+                </label>
+              </div>
+
+              {/* Create Token Button */}
+              {tokenMintAddress ? (
+                <div className="form-group">
+                  <a
+                    href={`https://solscan.io/account/${tokenMintAddress}?cluster=devnet`}
+                    target="_blank"
+                    className="btn btn-primary btn-block "
+                  >
+                    View Explorer
+                  </a>
+                </div>
+              ) : balance < 2 ? (
+                <div className="form-group">
+                  <button className="btn btn-primary btn-block ">
+                    Balance {balance} Sol, required minimum 2 Sol
+                  </button>
+                </div>
+              ) : (
+                <div className="form-group">
+                  <button
+                    onClick={() => createToken(token)}
+                    className="btn btn-primary btn-block "
+                  >
+                    Create token (Fee {SOLANA_FEE} Sol)
+                  </button>
+                </div>
+              )}
               <div className="text-center notice">
                 By creating token you agree to our
                 <a href="/pricing" target="_blank">
